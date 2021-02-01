c0d4734aa2965b3a111093d8fc0c412fc058694d
Luis Parrado <luisprmat@gmail.com>
2021-01-31 21:13:28 -0500

    Showing list of categories

---
+++ app/JsonApi/Categories/Schema.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\JsonApi\Categories;
+
+use Neomerx\JsonApi\Schema\SchemaProvider;
+
+class Schema extends SchemaProvider
+{
+
+    /**
+     * @var string
+     */
+    protected $resourceType = 'categories';
+
+    /**
+     * @param Category $resource
+     *      the domain record being serialized.
+     * @return string
+     */
+    public function getId($resource)
+    {
+        return (string) $resource->getRouteKey();
+    }
+
+    /**
+     * @param Category $resource
+     *      the domain record being serialized.
+     * @return array
+     */
+    public function getAttributes($resource)
+    {
+        return [
+            'name' => $resource->name,
+            'slug' => $resource->slug,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ JsonApi::register('v1')->routes(function($api){
         $api->hasOne('authors')->except('replace');
     });
     $api->resource('authors')->only('index', 'read');
+    $api->resource('categories')->only('index', 'read');
 });
